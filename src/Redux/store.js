1e4ee81e7dc355db7207be88fae0944c34dafdd3
Gautam Kumar <gkomsai788@gmail.com>
2022-08-23 14:13:42 +0530

    Basic project setup done

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,1 +1,18 @@
-// code here
+import {
+  legacy_createStore,
+  applyMiddleware,
+  compose,
+  combineReducers,
+} from "redux";
+import thunk from "redux-thunk";
+import { reducer as authReducer } from "./auth/reducer";
+import { reducer as appReducer } from "./app/reducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducers = combineReducers({ authReducer, appReducer });
+
+export const store = legacy_createStore(
+  rootReducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
