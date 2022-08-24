875b01ce3403e62783eadd5b801c6f4a31e865a4
Gautam Kumar <gkomsai788@gmail.com>
2022-08-24 12:38:42 +0530

    Navabr completed

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -5,13 +5,15 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Pricing from "../pages/pricing/Pricing";
 import SignUp from "../pages/signUp/SignUp";
+import Tour from "../pages/tour/Tour";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/pricing" element={<Pricing />} />
-      <Route path="/demo" element={<Demo />} />
+      <Route path="/request-a-demo" element={<Demo />} />
+      <Route path="/tour" element={<Tour />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
