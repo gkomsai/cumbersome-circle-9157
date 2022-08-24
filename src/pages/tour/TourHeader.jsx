61bce4ea33931815a798100545d552fd6bc73faf
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2022-08-24 20:58:37 +0530

    i

---
+++ src/pages/tour/TourHeader.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Box, Button, Image, Input, Spacer, Stack,Text} from "@chakra-ui/react"
+import ReactPlayer from 'react-player'
+import styles from "./tour.module.css";
+
+
+const TourHeader = () => {
+  return (
+    <Box>
+    <Box marginLeft='5rem'>
+        <Stack direction="row" border='1px solid'>
+            <Box w='40%'>
+                <Box w='80%' marginTop='2rem'><Text fontWeight='500' fontSize='5xl'>Everything your team needs to run more profitable projects</Text>
+                <Stack direction='row'>
+                <Input placeholder="Work email"/>
+                <Button colorScheme='green'>Get started</Button>
+                </Stack>
+                </Box> 
+            </Box>
+            <Spacer/>
+            <Box >
+                <Image boxSize="30rem" src='https://blog-cdn.everhour.com/assets/images/new-design/illustrations/primary-illustrations/all-features-3.webp'/>
+            </Box>
+        </Stack>
+        </Box>
+        <Box m="0 5rem 0 5rem">
+            <Text fontSize="3xl" fontWeight='500'>Time management</Text>
+            <Text fontSize='2xl'>Track time spent on projects, hours of work, vacations or sick days</Text>
+            <ReactPlayer classname="youtube" controls url="https://youtu.be/jSoBagh6eIs"/>
+
+        </Box>
+      
+    
+    </Box>
+  )
+}
+
+export default TourHeader
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -5,12 +5,14 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Pricing from "../pages/pricing/Pricing";
 import SignUp from "../pages/signUp/SignUp";
+import Tour from "../pages/tour/Tour";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/pricing" element={<Pricing />} />
+      <Route path="/tour" element={<Tour />} />
       <Route path="/demo" element={<Demo />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
