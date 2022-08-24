37d4f8b98d169e8e5283d6a2d423e8b03d6f96e3
GovindLakhotiya <99141948+Govindlakhotiya@users.noreply.github.com>
2022-08-24 20:50:49 +0530

    LoginSignup page done

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,10 +1,123 @@
-import React from 'react'
-import styles from "./signUp.module.css";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+ import {useNavigate} from "react-router";
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+export default function SignupCard() {
+
+  var signupArr = JSON.parse(localStorage.getItem("signup")) || [];
+
+  const [showPassword, setShowPassword] = useState({
+    email: "", 
+    password: "", 
+  });
+
+  const handform = (e) => { 
+    let { name, value } = e.target; 
+    setShowPassword({ ...showPassword, [name]: value }); 
+  };
+
+  const handlesub = (e) => { 
+    e.preventDefault(); 
+    signupArr.push(showPassword); 
+    localStorage.setItem("signup", JSON.stringify(signupArr)); 
+     navigate("/login"); 
+    console.log(showPassword)
+  };
+const navigate=useNavigate()
+return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')} isRequired>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign up in to Everhour
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" isRequired />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" isRequired/>
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email"
+              onChange={handform}  name="email" value={showPassword.email}
+              isRequired />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'}
+                 onChange={handform} name="password"  value={showPassword.password}
+                 isRequired/>
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }} onClick={ handlesub}>
+                Sign up
+              </Button>
+            </Stack>
+            {/* <Stack pt={6}>
+              <Text align={'center'}>
+                Already a user? <Link to="login" color={'blue.400'}>Login</Link>
+              </Text>
+            </Stack> */}
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
-
-export default SignUp
