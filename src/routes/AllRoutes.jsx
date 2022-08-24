91211cc11466e57ef0ffb45103563df760abee07
Gautam Kumar <gkomsai788@gmail.com>
2022-08-24 08:30:53 +0530

    authReducer completed and routing file updated

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Demo from "../pages/demo/Demo";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Pricing from "../pages/pricing/Pricing";
+import SignUp from "../pages/signUp/SignUp";
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/demo" element={<Demo />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
