cc5d0e908fe59215672789157835819c27f6862f
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2022-08-26 15:11:09 +0530

    done

--- src/pages/tour/Tour.jsx
+++ src/pages/tour/Tour.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from "./tour.module.css";
 import Tourbuilding from './Tourbuilding';
 import TourHeader from './TourHeader';
+import Visuals from './Visuals';
 
 const Tour = () => {
   return (
     <div>
       <TourHeader/>
       <Tourbuilding/>
+      <Visuals/>
     </div>
   )
 }
