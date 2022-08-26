deced44bb18d1a91d436b7a40c407e1d3fc12ff2
Gautam Kumar <gkomsai788@gmail.com>
2022-08-26 18:45:05 +0530

    home page Bottom part completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,19 +3,26 @@ import React from 'react'
 import BottomSection from './carouselSection/BottomSection';
 import Carousel from './carouselSection/carousel';
 import TopSection from './carouselSection/TopSection';
+import ReviewSection from './ReviewSection/ReviewSection';
 
 
 
-import styles from "./home.module.css";
 
 const Home = () => {
-  return (
-    <Box>
-      <TopSection />
-      <Carousel />
-      <BottomSection / >
-    </Box>
+
+
+  return ( 
+  <Box >
+    <TopSection />
+    <Carousel />
+    <BottomSection / >
+      <ReviewSection />
+  </Box>
+
   )
-}
+  
+  }
 
 export default Home
+
+
