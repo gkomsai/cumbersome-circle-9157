76bcb09b883c01de75e5a4b4396e7e4d9fa47705
Gautam Kumar <gkomsai788@gmail.com>
2022-08-26 07:52:08 +0530

    carousel added and made it responsive

---
+++ src/pages/home/carousel.jsx
@@ -0,0 +1,66 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./carousel.css";
+import { data } from "./data";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+const PreviousBtn = (props) => {
+  // console.log(props);
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <IoIosArrowBack style={{ color: "blue", fontSize: "30px" }} />
+    </div>
+  );
+};
+const NextBtn = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <IoIosArrowForward style={{ color: "blue", fontSize: "30px" }} />
+    </div>
+  );
+};
+
+const Carousel = () => {
+  
+  return (
+    <div style={{marginLeft: "10%", marginTop: "10rem", border: "3px solid blue", width:"80%", magin: "auto" }} className="carousel">
+      <Slider
+        autoplay
+        autoplaySpeed={2000}
+        // dots
+        initialSlide={2}
+        infinite
+        prevArrow={<PreviousBtn />}
+        nextArrow={<NextBtn />}
+        customPaging={(i) => {
+          return (
+            <div>
+              <img
+                src={data[i]}
+                alt=""
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  objectFit: "cover",
+                  borderRadius: "10px",
+                }}
+              />
+            </div>
+          );
+        }}
+        dotsClass="slick-dots custom-indicator"
+      >
+        {data.map((item) => (
+          <div>
+            <img src={item} alt="" style={{ width: "100%", height: "80vh" }} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
