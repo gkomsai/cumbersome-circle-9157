20e9541c2c8b908971bdbe8a3885eefcd26cfc5e
Gautam Kumar <gkomsai788@gmail.com>
2022-08-27 23:47:50 +0530

    Error page completed

---
+++ src/pages/Error.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box, Button, Center, Heading, Image } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+const Error = () => {
+    const navigate = useNavigate();
+  return (
+    <>
+      <Center>
+        <Box mt={"4rem"}>
+          <Image
+            width="45rem"
+            src="https://st.depositphotos.com/1020482/3088/i/950/depositphotos_30884685-stock-photo-404-error-page-not-found.jpg"
+            alt=""
+          />
+        </Box>
+        <Heading color="red">Sorry, Page not found</Heading>
+        <br />
+        <Button onClick={()=>navigate("/")} >Go Back To HOME</Button>
+      </Center>
+    </>
+  );
+};
+
+export default Error;
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -7,6 +7,7 @@ import Reports from "../pages/dashboard/Reports";
 import Team from "../pages/dashboard/Team";
 import Time from "../pages/dashboard/Timer";
 import Demo from "../pages/demo/Demo";
+import Error from "../pages/Error";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Pricing from "../pages/pricing/Pricing";
@@ -27,6 +28,7 @@ const AllRoutes = () => {
       <Route path="/dashboard/team" element={<PrivateRoute><Team /></PrivateRoute>} />
       <Route path="/dashboard/time" element={<PrivateRoute><Time /></PrivateRoute>} />
       <Route path="/dashboard/reports" element={<PrivateRoute><Reports /></PrivateRoute>} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 };
