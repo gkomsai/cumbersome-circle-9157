441b263e4c000869bc45231156ed1e1852bee445
Gautam Kumar <gkomsai788@gmail.com>
2022-08-27 20:33:11 +0530

    login-signup file updated

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -13,87 +13,128 @@ import {
   Text,
   useColorModeValue,
   Link,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
- import {useNavigate} from "react-router";
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router";
 
 export default function SignupCard() {
+  const toast = useToast();
+  const navigate = useNavigate();
+  let signupArr = JSON.parse(localStorage.getItem("userDetails")) || [];
 
-  var signupArr = JSON.parse(localStorage.getItem("signup")) || [];
-
-  const [showPassword, setShowPassword] = useState({
-    email: "", 
-    password: "", 
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
   });
 
-  const handform = (e) => { 
-    let { name, value } = e.target; 
-    setShowPassword({ ...showPassword, [name]: value }); 
+  const handform = (e) => {
+    let { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
   };
 
-  const handlesub = (e) => { 
-    e.preventDefault(); 
-    signupArr.push(showPassword); 
-    localStorage.setItem("signup", JSON.stringify(signupArr)); 
-     navigate("/login"); 
-    console.log(showPassword)
+  const handlesub = (e) => {
+    e.preventDefault();
+    signupArr.push(userData);
+    localStorage.setItem("userDetails", JSON.stringify(signupArr));
+    setUserData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    });
+    toast({
+      title: "Signup Successfull !!!",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+      position: "top",
+    });
+    navigate("/login");
+    console.log(userData);
   };
-const navigate=useNavigate()
-return (
+
+  return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')} isRequired>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+      isRequired
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
             Sign up in to Everhour
           </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
+          <Text fontSize={"lg"} color={"gray.600"}>
             to enjoy all of our cool features ✌️
           </Text>
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
           <Stack spacing={4}>
             <HStack>
               <Box>
                 <FormControl id="firstName" isRequired>
                   <FormLabel>First Name</FormLabel>
-                  <Input type="text" isRequired />
+                  <Input
+                    name="firstName"
+                    type="text"
+                    placeholder="Enter first name"
+                    value={userData.firstName}
+                    onChange={handform}
+                    isRequired
+                  />
                 </FormControl>
               </Box>
               <Box>
                 <FormControl id="lastName">
                   <FormLabel>Last Name</FormLabel>
-                  <Input type="text" isRequired/>
+                  <Input
+                    name="lastName"
+                    type="text"
+                    placeholder="Enter Last name"
+                    value={userData.lastName}
+                    onChange={handform}
+                    isRequired
+                  />
                 </FormControl>
               </Box>
             </HStack>
             <FormControl id="email" isRequired>
               <FormLabel>Email address</FormLabel>
-              <Input type="email"
-              onChange={handform}  name="email" value={showPassword.email}
-              isRequired />
+              <Input
+                type="email"
+                onChange={handform}
+                name="email"
+                value={userData.email}
+                isRequired
+              />
             </FormControl>
             <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'}
-                 onChange={handform} name="password"  value={showPassword.password}
-                 isRequired/>
-                <InputRightElement h={'full'}>
+                <Input
+                  type={userData ? "text" : "password"}
+                  onChange={handform}
+                  name="password"
+                  value={userData.password}
+                  isRequired
+                />
+                <InputRightElement h={"full"}>
                   <Button
-                    variant={'ghost'}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    variant={"ghost"}
+                    onClick={() => userData((showPassword) => !showPassword)}
+                  >
+                    {userData ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
                 </InputRightElement>
               </InputGroup>
@@ -102,19 +143,24 @@ return (
               <Button
                 loadingText="Submitting"
                 size="lg"
-                bg={'blue.400'}
-                color={'white'}
+                bg={"blue.400"}
+                color={"white"}
                 _hover={{
-                  bg: 'blue.500',
-                }} onClick={ handlesub}>
+                  bg: "blue.500",
+                }}
+                onClick={handlesub}
+              >
                 Sign up
               </Button>
             </Stack>
-            {/* <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user? <Link to="login" color={'blue.400'}>Login</Link>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user?{" "}
+                <Link to="/login" color={"blue.400"}>
+                  Login
+                </Link>
               </Text>
-            </Stack> */}
+            </Stack>
           </Stack>
         </Box>
       </Stack>
