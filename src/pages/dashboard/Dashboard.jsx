734b823b3c5c5f90354f58ecc469f7b093de3290
Gautam Kumar <101813593+gkomsai@users.noreply.github.com>
2022-08-29 11:39:46 +0530

    Update Dashboard.jsx

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { Box,  } from "@chakra-ui/react";
+import React from "react";
+import Sidebar from "./Sidebar";
+
+
+const Dashboard = () => {
+  return (
+    <Box>
+    <Sidebar></Sidebar>
+    </Box>
+  );
+};
+
+export default Dashboard;
